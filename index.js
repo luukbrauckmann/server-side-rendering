15b2b04d29b4534c9d61a35f699edb249a3ac44c
Luuk <luukbrauckmann@gmail.com>
2023-03-14 21:27:44 +0100

    Styling fix Nieuwe menu knop styling CSS variables toegevoegd Data toegevoegd

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const port = 3000
 const routes = [
 	{ path: '/', title: 'Start', templateName: 'start' },
 	{ path: '/404', title: '404', templateName: 'not-found' },
-	{ path: '/toolboard', title: 'Toolboard', templateName: 'toolboard' },
+	{ path: '/principes', title: 'Principes', templateName: 'principes' },
 	{ path: '/account', title: 'Account', templateName: 'account' }
 ]
+
+const menu = [
+	{ href: '/', label: 'Start', icon: 'home' },
+	{ href: '/principes', label: 'Principes', icon: 'universal-access' },
+	{ href: '/account', label: 'Account', icon: 'user' },
+]
+
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -18,14 +25,12 @@ app.use(express.static('static'))
 app.get('/*', async (req, res) => {
 	const route = routes.find(route => route.path === req.path) || routes.find(route => route.path === '/404')
 
-	const url = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1/principles'
+	const url = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1/principes'
 	const data = await fetch(url)
 		.then(response => response.json())
 		.then(data => data)
 
-	res.render('index', { route, principes: data.principes })
+	res.render('index', { route, menu, principes: data.principes })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
